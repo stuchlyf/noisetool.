682de38e1643f77c88edf190ab4af64a57e690a1
Florian Stuchly <stuchlyf@stuchlyf.dev>
2023-11-25 12:12:52 +0100

    Add Timer-component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { BlobGroup } from "@/components/blobGroup";
 import { Toolbar } from "@/components/toolbar";
 import { cookies } from "next/headers";
+import Timer from "@/components/timer";
 
 export default function Page() {
   const volumeSsr = cookies().get("volume")?.value;
 
   return (
     <>
-      <div className={'flex justify-center'}>
+      <div className={'flex items-center gap-8 flex-col'}>
         <BlobGroup />
+        <Timer />
       </div>
       <Toolbar volumeSsr={volumeSsr ? parseInt(volumeSsr) : undefined} />
     </>
