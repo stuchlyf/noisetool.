e2f6b2ce7af94220fb9c7fe302c58dc658e0830e
Florian Stuchly <stuchlyf@stuchlyf.dev>
2023-12-01 19:47:21 +0100

    Adjust component to use `useTimeDifferenceToNow`

--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -1,48 +1,30 @@
 'use client';
 import React, { useMemo } from "react";
 import { useAudioStore } from "@/store";
-import { type Interval, intervalToDuration } from "date-fns";
-import { useNow } from "@/lib/hooks";
+import { useTimeDifferenceToNow } from "@/lib/hooks";
 import { cn } from "@/lib/utils";
 
 export default function Timer() {
   const noise = useAudioStore(state => state.noise);
-  const now = useNow();
 
   const {
-    hoursActive,
-    minutesActive,
-    secondsActive
-  } = useMemo(() => {
-    if (!noise) return {hoursActive: undefined, minutesActive: undefined, secondsActive: undefined};
-
-    const interval = {
-      start: noise.startedAt,
-      end: now
-    } satisfies Interval;
-
-    const duration = intervalToDuration(interval);
-
-    return {
-      hoursActive: duration.hours,
-      minutesActive: duration.minutes,
-      secondsActive: duration.seconds
-    }
-  }, [noise, now]);
+    result: timeDifference,
+    isError: timeDifferenceIsError
+  } = useTimeDifferenceToNow(noise?.startedAt);
 
   return (
     <div className={cn(
       `grid grid-flow-col gap-5 text-center auto-cols-max text-opacity-70 select-none transition-opacity opacity-0`,
-      noise && 'opacity-70'
+      !timeDifferenceIsError && 'opacity-70'
     )}>
       <div className="flex flex-col">
-        <CountdownNumber value={hoursActive} />hours
+        <CountdownNumber value={timeDifference?.hours} />hours
       </div>
       <div className="flex flex-col">
-        <CountdownNumber value={minutesActive} />min
+        <CountdownNumber value={timeDifference?.minutes} />min
       </div>
       <div className="flex flex-col">
-        <CountdownNumber value={secondsActive} />sec
+        <CountdownNumber value={timeDifference?.seconds} />sec
       </div>
     </div>
   );
